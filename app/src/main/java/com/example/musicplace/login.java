aef72793b5ea2a29f0ee779c49a95516fbb692e0
jucheolkang <jucheol9877@naver.com>
2024-05-21 01:39:37 +0900

    feat : login -> start 화면 이동 기능 추가

--- app/src/main/java/com/example/musicplace/login.java
+++ app/src/main/java/com/example/musicplace/login.java
@@ -1,6 +1,10 @@
 package com.example.musicplace;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class login extends AppCompatActivity {
+
+    private ImageButton back_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,16 @@ public class login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        back_login = (ImageButton) findViewById(R.id.back_login);
+        back_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, start.class);
+                startActivity(intent);
+            }
         });
     }
 }
