9d57da77296afcb7b1d6016984d4a4b323eafb61
jucheolkang <jucheol9877@naver.com>
2024-05-22 01:40:14 +0900

    feat : 아이디, 비밀번호 찾기 화면 추가

--- app/src/main/java/com/example/musicplace/findId.java
+++ app/src/main/java/com/example/musicplace/findId.java
@@ -3,7 +3,6 @@ package com.example.musicplace;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -12,16 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
-
+public class findId extends AppCompatActivity {
     private ImageButton back_login;
-    private Button forgetId, forgetPw;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_find_id);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -33,29 +29,7 @@ public class login extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(login.this, start.class);
-                startActivity(intent);
-            }
-        });
-
-
-        forgetId = (Button) findViewById(R.id.forgetId);
-        forgetId.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(login.this, findId.class);
-                startActivity(intent);
-            }
-        });
-
-
-        forgetPw = (Button) findViewById(R.id.forgetPw);
-        forgetPw.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(login.this, findPw.class);
+                Intent intent = new Intent(findId.this, login.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/musicplace/findPw.java
+++ app/src/main/java/com/example/musicplace/findPw.java
@@ -3,7 +3,6 @@ package com.example.musicplace;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -12,50 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class findPw extends AppCompatActivity {
 
     private ImageButton back_login;
-    private Button forgetId, forgetPw;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_find_pw);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+
         back_login = (ImageButton) findViewById(R.id.back_login);
         back_login.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(login.this, start.class);
-                startActivity(intent);
-            }
-        });
-
-
-        forgetId = (Button) findViewById(R.id.forgetId);
-        forgetId.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(login.this, findId.class);
-                startActivity(intent);
-            }
-        });
-
-
-        forgetPw = (Button) findViewById(R.id.forgetPw);
-        forgetPw.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(login.this, findPw.class);
+                Intent intent = new Intent(findPw.this, login.class);
                 startActivity(intent);
             }
         });
